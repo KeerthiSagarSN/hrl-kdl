70874fd44de6c8c7e19c64f0fef77877ea29ba24
Keerthi Sagar <keerthisagar1989n6@gmail.com>
2022-11-15 15:40:35 +0000

    Python3 compatible - Relative import of kdl_parser

--- pykdl_utils/build/lib/pykdl_utils/kdl_kinematics.py
+++ pykdl_utils/build/lib/pykdl_utils/kdl_kinematics.py
@@ -38,8 +38,7 @@ import rospy
 from sensor_msgs.msg import JointState
 import hrl_geom.transformations as trans
 from hrl_geom.pose_converter import PoseConv
-# Using relative import here --->> Need to install with python3 later
-from .kdl_parser import kdl_tree_from_urdf_model
+from kdl_parser import kdl_tree_from_urdf_model
 from urdf_parser_py.urdf import Robot
 
 def create_kdl_kin(base_link, end_link, urdf_filename=None, description_param="/robot_description"):
